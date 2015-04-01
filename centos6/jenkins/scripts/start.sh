8ed3697e0ce3a873401357bed1ec2c15017fe6e0
Janusz Slota <janusz.slota@allsaints.com>
2015-04-01 15:48:55 +0100

    supervisrd

--- centos6/jenkins/scripts/start.sh
+++ centos6/jenkins/scripts/start.sh
@@ -2,9 +2,10 @@
 
 rm -rf /var/lib/mysql
 /usr/bin/mysql_install_db --user=mysql --ldata=/var/lib/mysql
-/usr/bin/mysqld_safe & sleep 1s
+/usr/bin/mysqld_safe & sleep 5s
 
 mysql -uroot -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON development_hr.* TO 'root'@'%'; FLUSH PRIVILEGES;"
 killall mysqld
+sleep 5s
 echo "Starting supervisord"
 supervisord -n
