0cb390d351daa20ba5154f690fa019316be53121
Janusz Slota <janusz.slota@allsaints.com>
2015-04-01 16:15:42 +0100

    new approach

--- centos6/jenkins/scripts/start.sh
+++ centos6/jenkins/scripts/start.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-rm -rf /var/lib/mysql
-/usr/bin/mysql_install_db --user=mysql --ldata=/var/lib/mysql
-/usr/bin/mysqld_safe & sleep 10s
-
+service mysqld start
 mysql -uroot -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON development_hr.* TO 'root'@'%'; FLUSH PRIVILEGES;"
-killall mysqld
-sleep 10s
-echo "Starting supervisord"
-supervisord -n
